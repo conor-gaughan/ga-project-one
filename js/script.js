53607ab323d2a2c49787c48c67a622cbfef6598d
Conor <cgaughan241@gmail.com>
2020-10-14 22:46:55 -0400

    company info js complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,70 @@
 // CONSTANTS AND VARIABLES
 
+const API_KEY = 'bu3p71v48v6up0bi1v20'
+const BASE_URL = 'https://finnhub.io/api/v1/'
+
+// Price Data https://finnhub.io/api/v1/quote?symbol=AAPL&token=bu3p71v48v6up0bi1v20
+
+let companyInfo, userInput;
 
 
 // CACHED ELEMENT REFERENCES
+const input = document.querySelector('input[type="text"]')
+const formEl = document.querySelector('form')
+
+// COMPANY INFO
+
+const title = document.querySelector('.name')
+const ticker = document.querySelector('.ticker')
+const exchange = document.querySelector('.exchange')
+const url = document.querySelector('.company-url')
+const image = document.querySelector('.logo')
+const industry = document.querySelector('.industry')
+
+ 
+
 
 
 
 // EVENT LISTENERS
 
+formEl.addEventListener('submit', handleGetData)
+
 
 
 // FUNCTIONS
+
+
+
+
+
+
+
+function handleGetData(e) {
+    e.preventDefault()
+    userInput = input.value
+    fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${userInput}&token=bu3p71v48v6up0bi1v20`)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        companyInfo = data
+        render()
+  
+    })
+    .catch(function() {
+        console.log('error message')
+    })
+}
+
+function render() {
+    title.textContent = `Company Name: ${companyInfo.name}`;
+    ticker.textContent = companyInfo.ticker;
+    exchange.textContent = companyInfo.exchange;
+    url.setAttribute('href', `${companyInfo.weburl}`);
+    industry.textContent = companyInfo.finnhubIndustry;
+    image.setAttribute('src', companyInfo.logo)
+}
+
+
+
+
+// Have the entire table in a map function to display on page. do in index html first and style
