103d3408ca408e728b069735389bbb320dfe0d57
Conor <cgaughan241@gmail.com>
2020-10-17 18:04:03 -0400

    data in and styling

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const BASE_URL = 'https://finnhub.io/api/v1/'
 
 // Price Data https://finnhub.io/api/v1/quote?symbol=AAPL&token=bu3p71v48v6up0bi1v20
 
-let companyInfo, userInput;
+let companyInfo, userInput, peers, table, price;
 
 
 // CACHED ELEMENT REFERENCES
@@ -21,7 +21,19 @@ const url = document.querySelector('.company-url')
 const image = document.querySelector('.logo')
 const industry = document.querySelector('.industry')
 const widget = document.querySelector('script[type="text/javascript"]')
-const cards = document.querySelector('.related-cards')
+const cards = document.querySelector('.related-cards div')
+
+const current = document.querySelector('.current')
+const high = document.querySelector('.high')
+const low = document.querySelector('.low')
+const open = document.querySelector('.open')
+const previousClose = document.querySelector('.previous-close')
+
+
+
+// pricing
+
+const pricing = document.querySelector('.pricing')
 
 
 // EVENT LISTENERS
@@ -37,18 +49,36 @@ formEl.addEventListener('submit', handleGetData)
 function handleGetData(e) {
     e.preventDefault()
     userInput = input.value
+    companyData()
+    pricingData()
+    cardData()
+
+}
+
+function companyData() {
     fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${userInput}&token=bu3p71v48v6up0bi1v20`)
     .then((resp) => resp.json())
     .then(function(data) {
         companyInfo = data
-        console.log(data)
-        cardData()
+        // console.log('company info:', companyInfo)
+        // render()
         tableData()
-        render()
     })
-    .catch(function() {
-        console.log('error message')
+}
+
+function pricingData() {
+    fetch(`https://finnhub.io/api/v1/quote?symbol=${userInput}&token=bu3p71v48v6up0bi1v20`)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        price = data
+        console.log('PRICING', price)
+        
+        // for (const [key, value] of Object.entries(price)) {
+        //     console.log(`${key}: ${value}`);
+        //   }
+        // render()
     })
+
 }
 
 function cardData() {
@@ -57,7 +87,7 @@ function cardData() {
     .then(function(data) {
         peers = data
         // console.log(data)
-        render()
+        // render()
         })
 }
 
@@ -65,21 +95,53 @@ function tableData() {
     fetch(`https://finnhub.io/api/v1/stock/metric?symbol=${userInput}&metric=all&token=bu3p71v48v6up0bi1v20`)
     .then((resp) => resp.json())
     .then(function(data) {
-        // console.log(data)
+        table = data
         render()
-        })
+        console.log('TABLE DATA', table)
+    })
 }
 
 function generateUI() {
     return peers.map(function(company) {
-        // console.log('COMPANY', company)
         return `
         <article class="card">
-        <h3>${company}</h3>
+        <p>${company}</p>
     </article>`
     })
-
 }
+
+function generatePricing() {
+    // for (const [key, value] of Object.entries(price)) {
+    //     console.log(`${key}: ${value}`);
+    //   }
+    let lookUp = {
+            "c": 'Current Price:',
+            "h": 'High:',
+            "l": 'Low:',
+            "o": 'Open:',
+            "pc": 'Previous Close: ',
+            "t": 'Timestamp: '
+    }
+    let innerUI = Object.entries(price)
+    innerUI.pop()
+    const html = innerUI.map(function(value) {
+        return `<p class="pricing-value">${lookUp[value[0]]} ${value[1]}</p>`
+    })
+    return `
+    <section class="pricing-main">
+    ${html.join('')}
+    </section>
+    `
+}
+
+// function pricingRender() {
+//     current.textContent = `Current price: ${price.c.toFixed(2)}`
+//     high.textContent = `High: ${price.h.toFixed(2)}`
+//     low.textContent = `Low: ${price.l.toFixed(2)}`
+//     open.textContent = `Open: ${price.o.toFixed(2)}`
+//     previousClose.textContent = `Previous Close: ${price.pc.toFixed(2)}`
+// }
+
 
 function render() {
     title.textContent = companyInfo.name;
@@ -87,9 +149,11 @@ function render() {
     exchange.textContent = companyInfo.exchange;
     url.setAttribute('href', `${companyInfo.weburl}`);
     industry.textContent = companyInfo.finnhubIndustry;
-    image.setAttribute('src', companyInfo.logo)
-    cards.innerHTML = generateUI().join('')
+    image.setAttribute('src', companyInfo.logo);
+    cards.innerHTML = generateUI().join('');
+    pricing.innerHTML = generatePricing();
 }
+
 
 
 
